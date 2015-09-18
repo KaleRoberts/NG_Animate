559c2f87f0aa703f1c8ddec3b634782a8b30712c
kroberts <kroberts@netspend.com>
2015-09-18 15:24:36 -0500

    Working on learning about Angular's ngAnimate module

---
+++ app.js
@@ -0,0 +1,23 @@
+(function () {
+	"use strict";
+	
+	var app = angular.module('app', ['ngAnimate']);
+
+	app.controller('MainCtrl', [
+		'$scope',
+		function($scope) {
+
+			// $scope.test = function(){
+			// 	var self = this;
+			// 	self.showBoxOne = false;
+			// 	self.showBoxTwo = false;
+			// }
+			$scope.saturn = false;
+			$scope.neptune = false;
+
+
+		}]);
+
+
+
+}());
